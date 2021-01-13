b1fe18eae17743bb480fa7bc2681f17195ef5952
z.suhadolnik <suhadolnik@protonmail.com>
2021-01-13 11:49:19 +0100

    [VP]: Added cloudinary support

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-const { nextI18NextRewrites } = require('next-i18next/rewrites')
+const { nextI18NextRewrites } = require("next-i18next/rewrites");
 
-const localeSubpaths = {}
+const localeSubpaths = {};
 
 module.exports = {
   rewrites: async () => nextI18NextRewrites(localeSubpaths),
   publicRuntimeConfig: {
     localeSubpaths,
   },
-}
+  images: {
+    loader: "cloudinary",
+    path: "https://res.cloudinary.com/zanedev/",
+  },
+};
--- pages/product.js
+++ pages/product.js
@@ -1,9 +1,22 @@
-import Layout from '../components/layout'
+import { Image } from "cloudinary-react";
+import { SRLWrapper } from "simple-react-lightbox";
+import Layout from "../components/layout";
+
+const images = [1, 2];
 
 export default function Product() {
   return (
     <Layout>
       <div>PRODUCT</div>
+      <SRLWrapper>
+        {images.map((img) => (
+          <Image
+            key={img}
+            cloudName="zanedev"
+            publicId={`product/product_${img}`}
+          />
+        ))}
+      </SRLWrapper>
     </Layout>
-  )
+  );
 }
