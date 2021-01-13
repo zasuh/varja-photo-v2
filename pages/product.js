d7b2b405298b7718115c27e3466ad794aa55062c
z.suhadolnik <suhadolnik@protonmail.com>
2021-01-13 12:10:17 +0100

    [VP]: Added image width and grid layout

--- pages/product.js
+++ pages/product.js
@@ -2,20 +2,23 @@ import { Image } from "cloudinary-react";
 import { SRLWrapper } from "simple-react-lightbox";
 import Layout from "../components/layout";
 
-const images = [1, 2];
+const images = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11];
 
 export default function Product() {
   return (
     <Layout>
       <div>PRODUCT</div>
       <SRLWrapper>
-        {images.map((img) => (
-          <Image
-            key={img}
-            cloudName="zanedev"
-            publicId={`product/product_${img}`}
-          />
-        ))}
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((img) => (
+            <Image
+              key={img}
+              width="300"
+              cloudName="zanedev"
+              publicId={`product/${img}`}
+            />
+          ))}
+        </div>
       </SRLWrapper>
     </Layout>
   );
