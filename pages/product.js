0ac57a7ba034764384c3743530e1dedc46b2a9fa
Zane Suhadolnik <zanesuhi@gmail.com>
2021-01-23 08:26:41 +0100

    Commit for merge

--- pages/product.js
+++ pages/product.js
@@ -1,9 +1,26 @@
+import Image from 'next/image'
+import { SRLWrapper } from 'simple-react-lightbox'
 import Layout from '../components/layout'
+
+const indexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 export default function Product() {
   return (
     <Layout>
-      <div>PRODUCT</div>
+      <SRLWrapper>
+        <div className="grid grid-cols-3 gap-4">
+          {indexes.map(index => (
+            <Image
+              key={index}
+              src={`/product/${index}.jpg`}
+              layout="fixed"
+              width={400}
+              height={400}
+              quality={100}
+            />
+          ))}
+        </div>
+      </SRLWrapper>
     </Layout>
   )
 }
